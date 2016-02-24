b964c0d7869095f6ccd8c350ae2168f1ccb84215
jotne-aet-git <aet@jotne.com>
2016-02-24 18:05:51 +0100

    Safety copy

---
+++ ee1Server/webapps/e3-bimapi-a/test.js
@@ -0,0 +1,26 @@
+[{
+		"model_url" : "http://localhost:8080/e3-bimapi-a/E3ReposServlet/eee-repos/0.3/models/006M0K2HuHvO00y1_lMzwL?EDMSESSIONID=AUTO_LOGIN",
+		"model_meta_data" : {
+			"model_version" : "V1",
+			"domain_name" : "ArK",
+			"model_name" : "3302 Andabu",
+			"project_id" : "FM",
+			"model_type" : "IFC2X3",
+			"schema_url" : "http://example.com/ifc2x3",
+			"description" : "--missing--",
+			"model_guid" : "006M0K2HuHvO00y1_lMzwL"
+		}
+	}, {
+		"model_url" : "http://localhost:8080/e3-bimapi-a/E3ReposServlet/eee-repos/0.3/models/31qNw02RaHvO00y1_lMzwL?EDMSESSIONID=AUTO_LOGIN",
+		"model_meta_data" : {
+			"model_version" : "V2",
+			"domain_name" : "ArK",
+			"model_name" : "3302 Andabu",
+			"project_id" : "FM",
+			"model_type" : "IFC2X3",
+			"schema_url" : "http://example.com/ifc2x3",
+			"description" : "--missing--",
+			"model_guid" : "31qNw02RaHvO00y1_lMzwL"
+		}
+	}
+]
